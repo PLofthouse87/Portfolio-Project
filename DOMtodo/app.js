2fd356259fb184278f04c331ec8ff850191eee81
PLofthouse87 <paul.lofthouse@gmail.com>
2025-03-17 18:05:24 +0000

    Added a submit button
    Added a submit button for the form.
    
    I also started creating variables to hold various DOM elements.

--- DOMtodo/app.js
+++ DOMtodo/app.js
@@ -1,2 +1,8 @@
 "use strict";
 //console.log("Hello World");
+
+//Creating variables to hold various elements of the DOM
+//Creating the variable domForm to hold the entire form element
+const domForm = document.forms.domToDoForm;
+//Creating the variable taskTitle to hold the contents of Task Title form element
+const taskTitle = domForm.elements.taskTitle;
